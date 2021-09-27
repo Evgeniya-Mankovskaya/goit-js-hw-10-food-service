7e636e2a046bdfbcbf9cd2b708c0126eee28d783
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-09-27 23:24:56 +0300

    hw-10

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import menu from './menu.json';
+import refs from './js/refs.js';
+import markUp from './templetes/markUp.handlebars';
+const { menuJS, themeSwitchToggle } = refs;
+let result = markUp(menu);
+menuJS.insertAdjacentHTML('beforeend', result);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeSwitchToggle.addEventListener('change', e => {
+  document.querySelector('body').classList.toggle(Theme.DARK);
+  document.querySelector('body').classList.toggle(Theme.LIGHT);
+  let theme = e.target.checked ? Theme.DARK : Theme.LIGHT;
+  localStorage.setItem('theme', theme);
+});
+
+let theme = localStorage.getItem('theme');
+
+if (!theme) {
+  theme = Theme.LIGHT;
+  localStorage.setItem('theme', theme);
+}
+document.querySelector('body').classList.add(theme);
+themeSwitchToggle.checked = theme === Theme.LIGHT ? false : true;
